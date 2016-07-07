38550aca7b8a8d242d339d67f1bcbbb7ed47a4aa
Adrinalin4ik <soild@mail.ru>
2016-07-07 22:01:27 +0300

    Fix

--- client/states/play.js
+++ client/states/play.js
@@ -104,7 +104,7 @@ Play.prototype = {
         var startY = (88 * Pathfinder.tileSize) + (Pathfinder.tileSize / 2);
 
         this.mainPlayer = new CharacterObj(this.game, startX, startY, true);
-        //this.game.camera.follow(this.mainPlayer.sprite);
+        this.game.camera.follow(this.mainPlayer.sprite);
         //this.game.camera.deadzone = new Phaser.Rectangle(this.mainPlayer.sprite.x, this.mainPlayer.sprite.y, 600, 400);
         this.game.camera.deadzone = new Phaser.Rectangle(this.game.camera.x+this.mainPlayer.sprite.width*2, 
                                                          this.game.camera.y+this.mainPlayer.sprite.height*2, 
@@ -217,7 +217,7 @@ Play.prototype = {
         this.updateCursorPosition();
         this.checkMainPlayerCollision();
 
-        var zone = this.game.camera.deadzone;
+        /*var zone = this.game.camera.deadzone;
         
 
         if (this.mainPlayer.sprite.x>zone.x+zone.width){
@@ -258,7 +258,7 @@ Play.prototype = {
         }
 
         red_zone.lineStyle(2, 0x000000, 1);
-        red_zone.drawRect(zone.x, zone.y, zone.width, zone.height);
+        red_zone.drawRect(zone.x, zone.y, zone.width, zone.height);*/
 
 
           if (cursors.left.isDown)
